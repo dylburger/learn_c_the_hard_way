7ad1bbddffa439c5b98af9ca7105fb6f864eb770
Dylan Sather <dylan.sather@gmail.com>
2016-04-30 15:24:25 -0700

    Example 9: playing around more with how to define character arrays

--- ex9.c
+++ ex9.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
     // This is a shortcut in C. If you set one element of the array, it'll fill the rest in with zeros
     // You can confirm by setting the below element to 1 (the rest will be 0's)
     int numbers[4] = {0};
-    char name[4] = {'a'};
+    char *name = "Zed";
 
     // first, print our numbers out raw
     printf("numbers: %d %d %d %d\n",
@@ -25,12 +25,6 @@ int main(int argc, char *argv[])
     numbers[1] = 2;
     numbers[2] = 3;
     numbers[3] = 4;
-
-    // setup the name
-    name[0] = 'Z';
-    name[1] = 'e';
-    name[2] = 'd';
-    name[3] = 'A';
 
     // then print them out initialized
     printf("numbers: %d %d %d %d\n",
