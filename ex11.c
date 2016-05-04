4cd25e229b5d976d7ddebe3f703f7d5dcf31863f
Dylan Sather <dylan.sather@gmail.com>
2016-05-04 17:26:50 +0000

    When adding elements of argv to states, make sure we fail to add elements beyond the size of states

--- ex11.c
+++ ex11.c
@@ -10,11 +10,15 @@ int main(int argc, char *argv[])
     };
     int num_states = 4;
 
-    int i = argc - 1;
-    while(i >= 0) {
+    int i = 0;
+    // Index to count the number of elements added to the states array, to make sure we don't add too many
+    int j = 0;
+    while(i < argc && j < num_states) {
         printf("arg %d: %s\n", i, argv[i]);
         states[i] = argv[i];
-        i--;
+        i++;
+        // Count the number of times we've added an element of argv to the states array, so we don't add to many
+        j++;
     }
 
     i = 0;
