4795075e1350c9bb351446202ae8805fd67ceeb5
Dylan Sather <dylan.sather@gmail.com>
2016-05-09 21:32:58 +0000

    Example 12: Initial commit

---
+++ ex12.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+
+    if (argc == 1) {
+        printf("You only have one argument. Not enough!\n");
+    } else if (argc > 1 && argc < 4) {
+        printf("Here are your arguments:\n");
+
+        // i will start at 0 and end at one less than argv. We'll increment i a final time, and on the 
+        // next iteration of the loop, i will fail to meet the test condition
+        for (i = 0; i < argc; i++) {
+            printf("%s ", argv[i]);
+        }
+
+        printf("\n");
+
+    } else {
+        printf("You have too many arguments.\n");
+    }
+
+    return EXIT_SUCCESS;
+}
