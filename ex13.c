8806854ed2bee7ae359e865d386e2dfdfb1973a6
Dylan Sather <dylan.sather@gmail.com>
2016-05-10 18:39:29 +0000

    Example 14: Initial commit

--- ex13.c
+++ ex13.c
@@ -2,41 +2,54 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2) {
+    if (argc != 2) {
         printf("ERROR: You need one argument.\n");
         // Abort execution
         return 1;
     }
 
     // Otherwise, we have an argument and we can move on
-    int i;
-    char letter;
+    int i = 0;
     // Test condition: as long as we have characters in the string (until we reach the nul-byte)
-    for (i = 0, letter = argv[1][0]; argv[1][i] != '\0'; i++) {
+    for (i = 0; argv[1][i] != '\0'; i++) {
+        char letter = argv[1][i];
 
-        letter = argv[1][i];
+        switch(letter) {
+            case 'a':
+            case 'A':
+                printf("%d: 'A'\n", i);
+                break;
 
-        if (letter == 'a' || letter == 'A') {
-            printf("%d: 'A'\n", i);
-        } else if (letter == 'e' || letter == 'E') {
-            printf("%d: 'E'\n", i);
-        } else if (letter == 'i' || letter == 'I') {
-            printf("%d: 'I'\n", i);
-        } else if (letter == 'o' || letter == 'O') {
-            printf("%d: 'O'\n", i);
-        } else if (letter == 'u' || letter == 'U') {
-            printf("%d: 'U'\n", i);
-        } else if ((letter == 'y' || letter == 'Y') && i > 2) {
-            printf("%d: 'Y'\n", i);
-        } else {
-            printf("%d: %c is not a vowel\n", i, letter);
-        }
-    }
+            case 'e':
+            case 'E':
+                printf("%d: 'E'\n", i);
+                break;
 
-    // Handle the rest of our arguments in argv[], if we have them
-    if (argc > 2) {
-        for (i = 2; i < argc; i++) {
-            printf("arg %d: %s\n", i, argv[i]);
+            case 'i':
+            case 'I':
+                printf("%d: 'I'\n", i);
+                break;
+
+            case 'o':
+            case 'O':
+                printf("%d: 'O'\n", i);
+                break;
+
+            case 'u':
+            case 'U':
+                printf("%d: 'U'\n", i);
+                break;
+
+            case 'y':
+            case 'Y':
+                if (i > 2) {
+                    // Y is only a vowel if it comes in at least the 3rd position
+                    printf("%d: 'Y'\n", i);
+                }
+                break;
+
+            default:
+                printf("%d: %c is not a vowel\n", i, letter);
         }
     }
 
