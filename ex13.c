6c06a622ed28b283c968460521908feefc7228a3
Dylan Sather <dylan.sather@gmail.com>
2016-05-10 18:25:29 +0000

    Example 13: Converting our switch statement to an if / else conditional

--- ex13.c
+++ ex13.c
@@ -16,42 +16,20 @@ int main(int argc, char *argv[])
 
         letter = argv[1][i];
 
-        switch(letter) {
-            case 'a':
-            case 'A':
-                printf("%d: 'A'\n", i);
-                break;
-
-            case 'e':
-            case 'E':
-                printf("%d: 'E'\n", i);
-                break;
-
-            case 'i':
-            case 'I':
-                printf("%d: 'I'\n", i);
-                break;
-
-            case 'o':
-            case 'O':
-                printf("%d: 'O'\n", i);
-                break;
-
-            case 'u':
-            case 'U':
-                printf("%d: 'U'\n", i);
-                break;
-
-            case 'y':
-            case 'Y':
-                if (i > 2) {
-                    // Y is only a vowel if it comes in at least the 3rd position
-                    printf("%d: 'Y'\n", i);
-                }
-                break;
-
-            default:
-                printf("%d: %c is not a vowel\n", i, letter);
+        if (letter == 'a' || letter == 'A') {
+            printf("%d: 'A'\n", i);
+        } else if (letter == 'e' || letter == 'E') {
+            printf("%d: 'E'\n", i);
+        } else if (letter == 'i' || letter == 'I') {
+            printf("%d: 'I'\n", i);
+        } else if (letter == 'o' || letter == 'O') {
+            printf("%d: 'O'\n", i);
+        } else if (letter == 'u' || letter == 'U') {
+            printf("%d: 'U'\n", i);
+        } else if ((letter == 'y' || letter == 'Y') && i > 2) {
+            printf("%d: 'Y'\n", i);
+        } else {
+            printf("%d: %c is not a vowel\n", i, letter);
         }
     }
 
