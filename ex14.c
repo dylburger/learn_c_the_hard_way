65367c37ab05a6f253fdabce93ec47e4ff061dc1
Dylan Sather <dylan.sather@gmail.com>
2016-05-10 18:51:49 +0000

    Example 14: Playing around with new string library, adding more arguments to function

--- ex14.c
+++ ex14.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 // forward declarations
-void print_letters(char arg[]);
+void print_letters(int len, char arg[]);
 
 void print_arguments(int argc, char *argv[])
 {
     int i = 0;
+    int len = 0;
 
     for (i = 0; i < argc; i++) {
-        print_letters(argv[i]);
+        len = strlen(argv[i]);
+        print_letters(len, argv[i]);
     }
 }
 
-void print_letters(char arg[])
+void print_letters(int len, char arg[])
 {
     int i = 0;
 
     // Keep iterating until we encounter a nul byte
-    for (i = 0; arg[i] != '\0'; i++) {
+    for (i = 0; i < len; i++) {
         char ch = arg[i];
 
         if (isalpha(ch) || isblank(ch)) {
